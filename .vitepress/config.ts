6e0b808b90c4099e7f8f119a978f4d6835336ba2
haseebn19 <haseeb.kn@outlook.com>
2026-02-01 02:07:38 -0500

    fix: add head configuration for favicon in VitePress setup

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -5,6 +5,9 @@ export default defineConfig({
   title: "ReQuizle Docs",
   description: "Documentation for ReQuizle",
   base: '/requizle-wiki/',
+  head: [
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/requizle-wiki/icon.svg' }],
+  ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     logo: '/icon.svg',
